9200e9c274f5cb74acabe7e46c02c3e54da77cf2
Valentine AKEM <valyakem@yahoo.co.uk>
2022-09-12 17:00:59 -0400

    version change

--- modules/vpc-endpoints/versions.tf
+++ modules/vpc-endpoints/versions.tf
@@ -1,10 +1,19 @@
-terraform {
-  required_version = ">= 0.13.1"
+# terraform {
+#   required_version = ">= 0.13.1"
 
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = ">= 3.28"
+#     }
+#   }
+# }
+
+terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.28"
+      source = "hashicorp/aws"
+      version = "= 3.74.2"
     }
   }
 }
