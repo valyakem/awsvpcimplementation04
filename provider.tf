818e08a6ca4e361e9b5bbb9c79e6d5147540f897
Valentine AKEM <valyakem@yahoo.co.uk>
2022-09-12 13:48:00 -0400

    updated kyes

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region  = "us-east-1"
-  aws_access_key_id="AKIATJUJQJNYU5N6LJKC"
-  aws_secret_access_key="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"
+  aws_access_key_id=AKIATJUJQJNYU5N6LJKC
+  aws_secret_access_key=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
 }
 
