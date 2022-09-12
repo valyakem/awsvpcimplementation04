7c70d93e72c35c5f2bf43b62c5e5a80beac8c06a
Valentine AKEM <valyakem@yahoo.co.uk>
2022-09-12 13:09:16 -0400

    updated iam

--- provider.tf
+++ provider.tf
@@ -1,10 +1,9 @@
 provider "aws" {
   region  = "us-east-1"
-   shared_credentials_file = "~/.aws/credentials"
   profile = "destination"
 
-  # assume_role {
-  #   role_arn     = "arn:aws:iam::226847902577:role/mainaccount-role"
-  # }
+  assume_role {
+    role_arn     = "arn:aws:iam::226847902577:role/mainaccount-role"
+  }
 }
 
