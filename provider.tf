18d4838d03a4835721bc29757153f7cedaded8b2
Valentine AKEM <valyakem@yahoo.co.uk>
2022-09-12 13:46:28 -0400

    updated keys to test

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region  = "us-east-1"
-  profile = "destination"
+  aws_access_key_id="AKIATJUJQJNYU5N6LJKC"
+  aws_secret_access_key="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"
 }
 
